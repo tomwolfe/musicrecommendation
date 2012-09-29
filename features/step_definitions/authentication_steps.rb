630f3359451c9729f6199fbadf30cb90b389b969
Thomas Wolfe <tomwolfe@gmail.com>
2012-09-29 05:09:09 -0500

    oops, forgot a not

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -7,7 +7,7 @@ When /I should( not)?see signed in items/ do |negate|
   Then %Q(I should#{negate} see the link "Sign Out" in the "header" section)
   negate ? Then %Q(I should see the link "Sign In" in the "header" section) : Then %Q(I should not see the link "Sign In" in the "header" section)
   negate ? Then %Q(I should see the link "Join" in the "header" section) : Then %Q(I should not see the link "Join" in the "header" section)
-  negate ? Then %Q(I should see "Get personalized") : Then %Q(I should not "Get personalized")
+  negate ? Then %Q(I should see "Get personalized") : Then %Q(I should not see "Get personalized")
   Then %Q(I should#{negate} see "Top Recommended Tracks")
   Then %Q(I should#{negate} see the link "Search for a track to rate")
   Then %Q(I should#{negate} see "Your Most Recent Ratings")

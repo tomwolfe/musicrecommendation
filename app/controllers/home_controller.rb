eaa4d10ee97abae54beabfec664ea0d2896d5f99
Thomas Wolfe <tomwolfe@gmail.com>
2012-10-15 11:39:52 -0500

    Update app/controllers/home_controller.rb
    use pluck rather than map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     else
       @ratings = current_user.ratings.order("created_at DESC").limit(10)
       unless @ratings.empty? or nil
-        not_yet_rated_tracks = Track.select("id").where('id not in (?)', current_user.ratings.select("track_id").map(&:track_id))
+        not_yet_rated_tracks = Track.select("id").where('id not in (?)', current_user.ratings.pluck(:track_id))
         @predictions = current_user.predictions.where('track_id in (?)', not_yet_rated_tracks).order("value DESC").limit(10)
       end
     end

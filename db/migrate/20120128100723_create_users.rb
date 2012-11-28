fe0cf1a302fb6ba175550d535e60b4b40503a7a8
Thomas Wolfe <tomwolfe@gmail.com>
2012-11-28 04:53:32 -0600

    Sessions#create uses find_by_email so it should be indexed
    find_by_x and where attributes should be indexed to avoid O(n) lookups and instead have O(1) (yay hashing)

--- db/migrate/20120128100723_create_users.rb
+++ db/migrate/20120128100723_create_users.rb
@@ -7,9 +7,13 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password_digest
       t.timestamps
     end
+    
+    add_index :users, :email
   end
 
   def self.down
     drop_table :users
+    
+    remove_index :users, :email
   end
 end

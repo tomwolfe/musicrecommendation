54f418f2d87eefc5ff4a862077e357f656c33546
Thomas Wolfe <tomwolfe@gmail.com>
2012-11-28 04:38:55 -0600

    Added a Fixme due to not using eager-loading
    I'll actually implement/test this later.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
 
   def index
     if current_user
+      # FIXME: in the home view we access the artist_name/etc of each ratings track.
+      # this is the N+1 problem http://guides.rubyonrails.org/active_record_querying.html#eager-loading-associations
+      # maybe current_user.ratings.includes(:track).where(blah).order(blah).limit(10)
+      # I'll test this later...
       @ratings = current_user.ratings.where("value IS NOT NULL").order("created_at DESC").limit(10)
       @unrated_predictions = current_user.ratings.where("value IS NULL").select("id, user_id, track_id, value, prediction, created_at, updated_at, abs(prediction-value) AS difference").order("prediction DESC").limit(10)
     else

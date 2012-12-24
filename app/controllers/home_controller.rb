14a99cf13a41da5f6160adbc835d64f33299fac1
Thomas Wolfe <tomwolfe@gmail.com>
2012-12-24 00:10:20 -0600

    ignore vim swp in git. separate home_controller actions for signedin/signedout (should make caching easier) added an index for 'created_at' since it's being used for sorting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,16 @@
 class HomeController < ApplicationController
-  skip_before_filter :authorize, only: [:index]
+	skip_before_filter :authorize, only: [:index]
 
-  # FIXME: separate index actions for logged_in and not_logged_in users (action cache for logged in/page cache for not_logged_in)
-  def index
-    if current_user
-      # FIXME: in the home view we access the artist_name/etc of each ratings track.
-      # this is the N+1 problem http://guides.rubyonrails.org/active_record_querying.html#eager-loading-associations
-      # maybe current_user.ratings.includes(:track).where(blah).order(blah).limit(10)
-      # I'll test this later...
-      # FIXME: index columns that sorts/orders are done on
-      @ratings = current_user.ratings.where("value IS NOT NULL").order("created_at DESC").limit(10)
-      @unrated_predictions = current_user.ratings.where("value IS NULL").select("id, user_id, track_id, value, prediction, created_at, updated_at, abs(prediction-value) AS difference").order("prediction DESC").limit(10)
-    else
-      @tracks = Track.order("average_rating DESC").limit(10)
-    end
-  end
+	def signedin
+			# FIXME: in the home view we access the artist_name/etc of each ratings track.
+			# this is the N+1 problem http://guides.rubyonrails.org/active_record_querying.html#eager-loading-associations
+			# maybe current_user.ratings.includes(:track).where(blah).order(blah).limit(10)
+			# I'll test this later...
+			@ratings = current_user.ratings.where("value IS NOT NULL").order("created_at DESC").limit(10)
+			@unrated_predictions = current_user.ratings.where("value IS NULL").select("id, user_id, track_id, value, prediction, created_at, updated_at, abs(prediction-value) AS difference").order("prediction DESC").limit(10)
+	end
+
+	def signedout
+		@tracks = Track.order("average_rating DESC").limit(10)
+	end
 end
--- db/migrate/20120131094538_create_ratings.rb
+++ db/migrate/20120131094538_create_ratings.rb
@@ -1,23 +1,25 @@
 class CreateRatings < ActiveRecord::Migration
-  def self.up
-    create_table :ratings do |t|
-      t.integer :user_id
-      t.integer :track_id
-      t.integer :value
-      t.float   :prediction
+	def self.up
+		create_table :ratings do |t|
+			t.integer :user_id
+			t.integer :track_id
+			t.integer :value
+			t.float		:prediction
 
-      t.timestamps
-    end
-    
-    add_index :ratings, :value
-    add_index :ratings, :user_id
-    add_index :ratings, :track_id
-  end
-  
-  def self.down
-    drop_table :ratings
-    remove_index :ratings, :value
-    remove_index :ratings, :user_id
-    remove_index :ratings, :track_id
-  end
+			t.timestamps
+		end
+
+		add_index	:ratings,	:created_at
+		add_index :ratings, :value
+		add_index :ratings, :user_id
+		add_index :ratings, :track_id
+	end
+	
+	def self.down
+		drop_table :ratings
+		remove_index :ratings, :created_at
+		remove_index :ratings, :value
+		remove_index :ratings, :user_id
+		remove_index :ratings, :track_id
+	end
 end

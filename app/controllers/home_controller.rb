45c0ed5f6551582ae59e5617df9be8e5ff0fac02
Thomas Wolfe <tomwolfe@gmail.com>
2013-01-04 05:54:52 -0600

    N+1 problem solved in signedin view (each rating would have to query for it's associated track [and it's artist_name/name])

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,9 @@ class HomeController < ApplicationController
 	skip_before_filter :authorize, only: [:signedout]
 
 	def signedin
-			# FIXME: in the home view we access the artist_name/etc of each ratings track.
-			# this is the N+1 problem http://guides.rubyonrails.org/active_record_querying.html#eager-loading-associations
-			# maybe current_user.ratings.includes(:track).where(blah).order(blah).limit(10)
-			# I'll test this later...
-			@ratings = current_user.ratings.where("value IS NOT NULL").order("created_at DESC").limit(10)
-			@unrated_predictions = current_user.ratings.where("value IS NULL").select("id, user_id, track_id, value, prediction, created_at, updated_at, abs(prediction-value) AS difference").order("prediction DESC").limit(10)
+			# .includes should solve the N+1 problem in the view http://guides.rubyonrails.org/active_record_querying.html#eager-loading-associations
+			@ratings = current_user.ratings.includes(:track).where("value IS NOT NULL").order("created_at DESC").limit(10)
+			@unrated_predictions = current_user.ratings.includes(:track).where("value IS NULL").select("id, user_id, track_id, value, prediction, created_at, updated_at, abs(prediction-value) AS difference").order("prediction DESC").limit(10)
 	end
 
 	def signedout

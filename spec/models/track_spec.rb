55e44bc7003ff8d3192769ce8c084b49bc1677ab
Thomas Wolfe <tomwolfe@gmail.com>
2013-01-04 05:39:30 -0600

    tests for get_track_from_musicbrainz passing

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -6,17 +6,16 @@ end
 
 describe Track do
 	before :each do
-		track = MusicBrainz::Model::Track.new("http://musicbrainz.org/track/ea0ad976-5750-4885-9f33-2c656dfc4a42", "bound for the floor")
-		track.artist = "local h"
-		@scored_collection = MusicBrainz::Model::ScoredCollection.new << track
+		@track = MusicBrainz::Model::Track.new("http://musicbrainz.org/track/ea0ad976-5750-4885-9f33-2c656dfc4a42", "bound for the floor")
+		@track.artist = "local h"
+		@scored_collection = MusicBrainz::Model::ScoredCollection.new << @track
 		Track::QUERY.stub(:get_tracks).and_return(@scored_collection)
-		Track::QUERY.stub(:get_track_by_id).and_return(track)
+		@first_entity_in_collection = @scored_collection.first.entity
 	end
 	
 	describe '.find_in_musicbrainz' do
 		before :each do
 			update_mbids
-			@first_entity_in_collection = @scored_collection.first.entity
 		end
 		it 'returns an array of Track objects of tracks found in musicbrainz that are not already in the database' do
 			Track.find_in_musicbrainz(@all_track_mbids, @first_entity_in_collection.title, @first_entity_in_collection.artist).first.name.should eq(@first_entity_in_collection.title)
@@ -37,8 +36,12 @@ describe Track do
 	end
 	
 	describe '.get_track_from_musicbrainz' do
-		it '' do
-			pending
+		it 'returns a Track object given a valid mbid' do
+			Track::QUERY.stub(:get_track_by_id).and_return(@track)
+			Track.get_track_from_musicbrainz(@first_entity_in_collection.id.uuid).name.should eq("bound for the floor") 
+		end
+		it 'raises an InvalidMBIDError given an invalid mbid' do
+			lambda { Track.get_track_from_musicbrainz("hi").should raise_error(MusicBrainz::Model::InvalidMBIDError) }
 		end
 	end
 end

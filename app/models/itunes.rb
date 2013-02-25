de9b9f2b87c80449767198b62041edeaba308123
Thomas Wolfe <tomwolfe@gmail.com>
2013-02-25 07:23:59 -0600

    itunes_spec now passes.

--- app/models/itunes.rb
+++ app/models/itunes.rb
@@ -1,21 +1,21 @@
 class Itunes
-	AFFILIATE_WRAPPER = "http://click.linksynergy.com/fs-bin/stat?id=CBIMl*gYY/8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1="
+	AFFILIATE_ID = "CBIMl*gYY/8"
+	AFFILIATE_WRAPPER = "http://click.linksynergy.com/fs-bin/stat?id=#{AFFILIATE_ID}&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1="
 	PARTNER_ID = "30"
 
 	def self.itunes_data(track)
-		results = ITunesSearchAPI.search(term: "#{track.artist_name} #{track.name}", media: "music", entity: "song")
-		results.empty? ? [] : results
+		ITunesSearchAPI.search(term: "#{track.artist_name} #{track.name}", media: "music", entity: "song")
 	end
 
-	def self.itunes_affiliate_data
-		base_data = self.itunes_track_data
+	def self.itunes_affiliate_data(track)
+		base_data = self.itunes_data(track)
 		return [] if base_data.empty?
-		base_data.collect { |hash| modify_url(hash) }
+		base_data.collect { |hash| self.modify_urls(hash) }
 	end
 	
-	def self.modify_url(hash)
+	def self.modify_urls(hash)
 		urls_to_modify = %w{artistViewUrl collectionViewUrl trackViewUrl}
-		urls_to_modify.each { |url| hash[url] = "#{AFFILIATE_WRAPPER}#{Track.escape_link(hash[url])}" }
+		urls_to_modify.each { |url| hash[url] = "#{AFFILIATE_WRAPPER}#{self.escape_link(hash[url])}" }
 		hash
 	end
 
--- spec/models/itunes_spec.rb
+++ spec/models/itunes_spec.rb
@@ -5,42 +5,52 @@ describe Itunes do
 		# FIXME: ugly, should probably use a json fixture or somesuch.
 		@tunes = [{"wrapperType"=>"track", "kind"=>"song", "artistId"=>37771, "collectionId"=>255169, "trackId"=>255076, "artistName"=>"Local H", "collectionName"=>"As Good as Dead", "trackName"=>"Bound for the Floor", "collectionCensoredName"=>"As Good as Dead", "trackCensoredName"=>"Bound for the Floor", "artistViewUrl"=>"https://itunes.apple.com/us/artist/local-h/id37771?uo=4", "collectionViewUrl"=>"https://itunes.apple.com/us/album/bound-for-the-floor/id255169?i=255076&uo=4", "trackViewUrl"=>"https://itunes.apple.com/us/album/bound-for-the-floor/id255169?i=255076&uo=4", "previewUrl"=>"http://a1301.phobos.apple.com/us/r1000/069/Music/9e/8b/ac/mzm.vykyoexn.aac.p.m4a", "artworkUrl30"=>"http://a1.mzstatic.com/us/r1000/000/Features/0a/df/97/dj.kdcejnrw.30x30-50.jpg", "artworkUrl60"=>"http://a5.mzstatic.com/us/r1000/000/Features/0a/df/97/dj.kdcejnrw.60x60-50.jpg", "artworkUrl100"=>"http://a5.mzstatic.com/us/r1000/000/Features/0a/df/97/dj.kdcejnrw.100x100-75.jpg", "collectionPrice"=>7.99, "trackPrice"=>1.29, "releaseDate"=>"1996-04-16T07:00:00Z", "collectionExplicitness"=>"notExplicit", "trackExplicitness"=>"notExplicit", "discCount"=>1, "discNumber"=>1, "trackCount"=>13, "trackNumber"=>3, "trackTimeMillis"=>222960, "country"=>"USA", "currency"=>"USD", "primaryGenreName"=>"Rock"}]
 		@track = FactoryGirl.build(:track)
+		ITunesSearchAPI.stub(:search).and_return(@tunes)
 	end
 	
-	describe '#itunes_links' do
-		before :each do
-			ITunesSearchAPI.stub(:search).and_return(@tunes)
+	describe '.itunes_data(track)' do
+		it 'returns an array of itunes data' do
+			Itunes.itunes_data(@track).should == @tunes
 		end
-		it 'returns an array of itunes links' do
-			@track.itunes_links.should == [@tunes.first["trackViewUrl"]]
+		it 'calls ITunesSearchAPI' do
+			ITunesSearchAPI.should_receive(:search)
+			Itunes.itunes_data(@track)
 		end
 	end
 
-	describe '#itunes_affiliate_links' do
-		before :each do
-			@should_return = ["http://click.linksynergy.com/fs-bin/stat?id=CBIMl*gYY/8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=https%253A%252F%252Fitunes.apple.com%252Fus%252Falbum%252Fbound-for-the-floor%252Fid255169%253Fi%253D255076%2526uo%253D4%2526partnerId%253D30"]
-			ITunesSearchAPI.stub(:search).and_return(@tunes)
+	describe '.itunes_affiliate_data(track)' do
+		it 'should call .itunes_links' do
+			#Itunes.should_receive(:itunes_data)
+			Itunes.should_receive(:itunes_data).and_return(@tunes)
+			Itunes.itunes_affiliate_data(@track)
 		end
-		it 'should call #itunes_links' do
-			@track.should_receive(:itunes_links).and_return(@should_return)
-			@track.itunes_affiliate_links
-		end
-		it 'should call .escape_link(link)' do
-			Track.should_receive(:escape_link)
-			@track.itunes_affiliate_links
+		it 'should call .modify_urls(hash)' do
+			Itunes.should_receive(:modify_urls)
+			Itunes.itunes_affiliate_data(@track)
 		end
 		it 'returns an array of itunes links with associated affiliate tracking' do
-			@track.itunes_affiliate_links.should == @should_return
+			Itunes.itunes_affiliate_data(@track).should == [{"wrapperType"=>"track", "kind"=>"song", "artistId"=>37771, "collectionId"=>255169, "trackId"=>255076, "artistName"=>"Local H", "collectionName"=>"As Good as Dead", "trackName"=>"Bound for the Floor", "collectionCensoredName"=>"As Good as Dead", "trackCensoredName"=>"Bound for the Floor", "artistViewUrl"=>"http://click.linksynergy.com/fs-bin/stat?id=CBIMl*gYY/8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=https%253A%252F%252Fitunes.apple.com%252Fus%252Fartist%252Flocal-h%252Fid37771%253Fuo%253D4%2526partnerId%253D30", "collectionViewUrl"=>"http://click.linksynergy.com/fs-bin/stat?id=CBIMl*gYY/8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=https%253A%252F%252Fitunes.apple.com%252Fus%252Falbum%252Fbound-for-the-floor%252Fid255169%253Fi%253D255076%2526uo%253D4%2526partnerId%253D30", "trackViewUrl"=>"http://click.linksynergy.com/fs-bin/stat?id=CBIMl*gYY/8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=https%253A%252F%252Fitunes.apple.com%252Fus%252Falbum%252Fbound-for-the-floor%252Fid255169%253Fi%253D255076%2526uo%253D4%2526partnerId%253D30", "previewUrl"=>"http://a1301.phobos.apple.com/us/r1000/069/Music/9e/8b/ac/mzm.vykyoexn.aac.p.m4a", "artworkUrl30"=>"http://a1.mzstatic.com/us/r1000/000/Features/0a/df/97/dj.kdcejnrw.30x30-50.jpg", "artworkUrl60"=>"http://a5.mzstatic.com/us/r1000/000/Features/0a/df/97/dj.kdcejnrw.60x60-50.jpg", "artworkUrl100"=>"http://a5.mzstatic.com/us/r1000/000/Features/0a/df/97/dj.kdcejnrw.100x100-75.jpg", "collectionPrice"=>7.99, "trackPrice"=>1.29, "releaseDate"=>"1996-04-16T07:00:00Z", "collectionExplicitness"=>"notExplicit", "trackExplicitness"=>"notExplicit", "discCount"=>1, "discNumber"=>1, "trackCount"=>13, "trackNumber"=>3, "trackTimeMillis"=>222960, "country"=>"USA", "currency"=>"USD", "primaryGenreName"=>"Rock"}]
 		end
 	end
 
 	describe '.escape_link(link)' do
 		it 'calls escape' do
 			CGI.should_receive(:escape).twice
-			Track.escape_link("https://")
+			Itunes.escape_link("https://")
 		end
 		it 'double escapes the link' do
-			Track.escape_link(@tunes.first["trackViewUrl"]).should == "https%253A%252F%252Fitunes.apple.com%252Fus%252Falbum%252Fbound-for-the-floor%252Fid255169%253Fi%253D255076%2526uo%253D4%2526partnerId%253D30"
+			Itunes.escape_link(@tunes.first["trackViewUrl"]).should == "https%253A%252F%252Fitunes.apple.com%252Fus%252Falbum%252Fbound-for-the-floor%252Fid255169%253Fi%253D255076%2526uo%253D4%2526partnerId%253D30"
+		end
+	end
+
+	describe '.modify_urls(hash)' do
+		it 'adds the AFFILIATE_WRAPPER and double escapes the link with partnerId appended' do
+			Itunes.modify_urls(@tunes.first).should == {"wrapperType"=>"track", "kind"=>"song", "artistId"=>37771, "collectionId"=>255169, "trackId"=>255076, "artistName"=>"Local H", "collectionName"=>"As Good as Dead", "trackName"=>"Bound for the Floor", "collectionCensoredName"=>"As Good as Dead", "trackCensoredName"=>"Bound for the Floor", "artistViewUrl"=>"http://click.linksynergy.com/fs-bin/stat?id=CBIMl*gYY/8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=https%253A%252F%252Fitunes.apple.com%252Fus%252Fartist%252Flocal-h%252Fid37771%253Fuo%253D4%2526partnerId%253D30", "collectionViewUrl"=>"http://click.linksynergy.com/fs-bin/stat?id=CBIMl*gYY/8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=https%253A%252F%252Fitunes.apple.com%252Fus%252Falbum%252Fbound-for-the-floor%252Fid255169%253Fi%253D255076%2526uo%253D4%2526partnerId%253D30", "trackViewUrl"=>"http://click.linksynergy.com/fs-bin/stat?id=CBIMl*gYY/8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=https%253A%252F%252Fitunes.apple.com%252Fus%252Falbum%252Fbound-for-the-floor%252Fid255169%253Fi%253D255076%2526uo%253D4%2526partnerId%253D30", "previewUrl"=>"http://a1301.phobos.apple.com/us/r1000/069/Music/9e/8b/ac/mzm.vykyoexn.aac.p.m4a", "artworkUrl30"=>"http://a1.mzstatic.com/us/r1000/000/Features/0a/df/97/dj.kdcejnrw.30x30-50.jpg", "artworkUrl60"=>"http://a5.mzstatic.com/us/r1000/000/Features/0a/df/97/dj.kdcejnrw.60x60-50.jpg", "artworkUrl100"=>"http://a5.mzstatic.com/us/r1000/000/Features/0a/df/97/dj.kdcejnrw.100x100-75.jpg", "collectionPrice"=>7.99, "trackPrice"=>1.29, "releaseDate"=>"1996-04-16T07:00:00Z", "collectionExplicitness"=>"notExplicit", "trackExplicitness"=>"notExplicit", "discCount"=>1, "discNumber"=>1, "trackCount"=>13, "trackNumber"=>3, "trackTimeMillis"=>222960, "country"=>"USA", "currency"=>"USD", "primaryGenreName"=>"Rock"}
+		end
+		it 'calls .escape_link' do
+			Itunes.should_receive(:escape_link).exactly(3).times
+			#Itunes.should_receive(:escape_link).with(kind_of(String))
+			Itunes.modify_urls(@tunes.first)
 		end
 	end
 end

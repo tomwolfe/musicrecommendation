bfd011b2f5a671d710a9068236a6b9af1d9c42a0
Thomas Wolfe <tomwolfe@gmail.com>
2014-01-09 20:45:32 -0600

    Rails 4 style include ActiveModel::Model, much cleaner

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,36 +1,23 @@
 class Search
-	# Rails 4 fixme: just need   include ActiveModel::Model   w/o initializer
-	include ActiveModel::Validations
-	include ActiveModel::Conversion
-	extend ActiveModel::Naming
+  include ActiveModel::Model
 
-	attr_accessor :track_name, :artist_name
+  attr_accessor :track_name, :artist_name
 
-	validates_presence_of :track_name, :artist_name
+  validates_presence_of :track_name, :artist_name
 
-	def initialize(attributes = {})
-		attributes.each do |name, value|
-			send("#{name}=", value)
-		end if attributes
-	end
-
-	def persisted?
-		false
-	end
-
-	def find_in_musicbrainz(existing_tracks)
-		mb_tracks = MusicBrainz::Recording.search(@track_name, @artist_name)
-		mb_tracks.delete_if { |track| existing_tracks.include? track[:id] }
-		ar_mb_tracks = create_tracks_array(mb_tracks)
-	end
-	
-	def create_tracks_array(mb_tracks)
-		ar_mb_tracks = Array.new
-		if mb_tracks.respond_to?(:each)
-			mb_tracks.each do |track|
-				ar_mb_tracks << Track.new(name: track[:title], artist_name: track[:artist], mb_id: track[:id], releases: track[:releases].to_s.tr('"', ''))
-			end
-		end
- 		ar_mb_tracks
-	end
+  def find_in_musicbrainz(existing_tracks)
+    mb_tracks = MusicBrainz::Recording.search(@track_name, @artist_name)
+    mb_tracks.delete_if { |track| existing_tracks.include? track[:id] }
+    ar_mb_tracks = create_tracks_array(mb_tracks)
+  end
+  
+  def create_tracks_array(mb_tracks)
+    ar_mb_tracks = Array.new
+    if mb_tracks.respond_to?(:each)
+      mb_tracks.each do |track|
+        ar_mb_tracks << Track.new(name: track[:title], artist_name: track[:artist], mb_id: track[:id], releases: track[:releases].to_s.tr('"', ''))
+      end
+    end
+    ar_mb_tracks
+  end
 end
